b1293a1570880f506fb8d04f21c57a1b8fb832cf
Alex <jobs@schelawski.de>
2016-04-17 20:52:01 +0200

    Project created

---
+++ MsSqlForXmlTests.WebApi/MsSqlForXmlTests.WebApi/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Xml;
+
+namespace MsSqlForXmlTests.WebApi.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [Route("Plants")]
+        public HttpResponseMessage GetPlants()
+        {
+            /*
+            SELECT TOP (200) * FROM Dictionary FOR XML RAW ('plaints'), ELEMENTS;
+            */
+            var doc = new XmlDocument();
+            doc.Load(System.AppDomain.CurrentDomain.BaseDirectory + @"App_Data\Plants.xml");
+
+            var jsonString = Newtonsoft.Json.JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.None, true);
+
+            var resp = new HttpResponseMessage()
+            {
+                Content = new StringContent(jsonString)
+            };
+
+            resp.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return resp;
+        }
+    }
+}
